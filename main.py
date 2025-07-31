fabb183f16d9480f417aef192108ec64b3d06959
david <miharisoadavidfils@gmail.com>
2025-07-31 08:53:02 +0300

    [ADD] answer of the question number 3 with his screenshoot

--- main.py
+++ main.py
@@ -17,3 +17,14 @@ def hello():
         status_code=200,
         media_type="text/html"
     )
+
+@app.get("/{full_path:path}")
+def catch_all(full_path: str):
+    with open("404.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+
+    return Response(
+        content=html_content,
+        status_code=404,
+        media_type="text/html"
+    )
