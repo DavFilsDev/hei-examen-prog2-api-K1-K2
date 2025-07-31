45031e6d58901efa11ae9cf85d80e134b4c0132f
david <miharisoadavidfils@gmail.com>
2025-07-31 08:33:21 +0300

    [ADD]: answer of the question number 2 whith his screenshoot

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import PlainTextResponse, Response
 
 app = FastAPI()
 
 @app.get("/ping", response_class=PlainTextResponse)
 def ping():
     return "pong"
+
+@app.get("/home")
+def hello():
+    with open("home.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+
+    return Response(
+        content=html_content,
+        status_code=200,
+        media_type="text/html"
+    )
